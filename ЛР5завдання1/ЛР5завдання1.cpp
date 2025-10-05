f25407880d7b41cc8c00104d7e820dceed62ee58
Victoria-Mykhailneko <vickysya2018@gmail.com>
2025-10-05 07:52:45 +0300

    Add project files.

---
+++ ЛР5завдання1/ЛР5завдання1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>     
+using namespace std;
+
+int main() {
+    double e = 0.001;    
+    double a, sum = 0;   
+    int n = 1;
+
+    do {
+        a = pow(-1, n + 1) * sin(n) / (n * n);
+        sum += a;
+        n++;
+    } while (fabs(a) >= e);  // fabs — модуль числа з плаваючою крапкою
+
+    cout << "Сума членів послідовності: " << sum << endl;
+    cout << "Кількість доданих членів: " << n - 1 << endl;
+    return 0;
+}
+
